d0e4f410d34a7b83d207c1e0d00c543098d3ad0d
DR <bsodoge@gmail.com>
2026-06-06 20:06:48 +0100

    Add project files.

---
+++ WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,249 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.guildsContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.friendsContainer = new System.Windows.Forms.FlowLayoutPanel();
+            this.friendsLabel = new System.Windows.Forms.Label();
+            this.serversLabel = new System.Windows.Forms.Label();
+            this.errorAuth = new System.Windows.Forms.Label();
+            this.messageContainer = new System.Windows.Forms.ListView();
+            this.Author = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.Message = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.label3 = new System.Windows.Forms.Label();
+            this.linkLabel1 = new System.Windows.Forms.LinkLabel();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.textBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.textBox1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(254)))));
+            this.textBox1.Location = new System.Drawing.Point(12, 25);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(425, 21);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Location = new System.Drawing.Point(456, 25);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 21);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Submit";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // guildsContainer
+            // 
+            this.guildsContainer.AutoScroll = true;
+            this.guildsContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.guildsContainer.Location = new System.Drawing.Point(421, 94);
+            this.guildsContainer.Name = "guildsContainer";
+            this.guildsContainer.Size = new System.Drawing.Size(363, 174);
+            this.guildsContainer.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(13, 280);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(0, 13);
+            this.label1.TabIndex = 3;
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(67)))), ((int)(((byte)(67)))), ((int)(((byte)(67)))));
+            this.button2.Enabled = false;
+            this.button2.FlatAppearance.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(1)))), ((int)(((byte)(1)))), ((int)(((byte)(1)))));
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button2.Location = new System.Drawing.Point(683, 274);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(101, 24);
+            this.button2.TabIndex = 4;
+            this.button2.Text = "Yes";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(624, 280);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(53, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Purge all?";
+            // 
+            // friendsContainer
+            // 
+            this.friendsContainer.AutoScroll = true;
+            this.friendsContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.friendsContainer.Location = new System.Drawing.Point(12, 94);
+            this.friendsContainer.Name = "friendsContainer";
+            this.friendsContainer.Size = new System.Drawing.Size(403, 174);
+            this.friendsContainer.TabIndex = 3;
+            // 
+            // friendsLabel
+            // 
+            this.friendsLabel.AutoSize = true;
+            this.friendsLabel.Location = new System.Drawing.Point(9, 78);
+            this.friendsLabel.Name = "friendsLabel";
+            this.friendsLabel.Size = new System.Drawing.Size(55, 13);
+            this.friendsLabel.TabIndex = 6;
+            this.friendsLabel.Text = "Friends: 0";
+            // 
+            // serversLabel
+            // 
+            this.serversLabel.AutoSize = true;
+            this.serversLabel.Location = new System.Drawing.Point(418, 78);
+            this.serversLabel.Name = "serversLabel";
+            this.serversLabel.Size = new System.Drawing.Size(57, 13);
+            this.serversLabel.TabIndex = 7;
+            this.serversLabel.Text = "Servers: 0";
+            // 
+            // errorAuth
+            // 
+            this.errorAuth.AutoSize = true;
+            this.errorAuth.ForeColor = System.Drawing.Color.Red;
+            this.errorAuth.Location = new System.Drawing.Point(118, 9);
+            this.errorAuth.Name = "errorAuth";
+            this.errorAuth.Size = new System.Drawing.Size(0, 13);
+            this.errorAuth.TabIndex = 9;
+            // 
+            // messageContainer
+            // 
+            this.messageContainer.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.messageContainer.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.messageContainer.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.Author,
+            this.Message});
+            this.messageContainer.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(254)))));
+            this.messageContainer.FullRowSelect = true;
+            this.messageContainer.GridLines = true;
+            this.messageContainer.HideSelection = false;
+            this.messageContainer.Location = new System.Drawing.Point(12, 304);
+            this.messageContainer.MultiSelect = false;
+            this.messageContainer.Name = "messageContainer";
+            this.messageContainer.Size = new System.Drawing.Size(772, 142);
+            this.messageContainer.TabIndex = 10;
+            this.messageContainer.UseCompatibleStateImageBehavior = false;
+            this.messageContainer.View = System.Windows.Forms.View.Details;
+            // 
+            // Author
+            // 
+            this.Author.Text = "Author";
+            // 
+            // Message
+            // 
+            this.Message.Text = "Message";
+            this.Message.Width = 712;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(9, 9);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(103, 13);
+            this.label3.TabIndex = 11;
+            this.label3.Text = "Authorization Code:";
+            // 
+            // linkLabel1
+            // 
+            this.linkLabel1.ActiveLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(141)))), ((int)(((byte)(211)))));
+            this.linkLabel1.AutoSize = true;
+            this.linkLabel1.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(254)))));
+            this.linkLabel1.Location = new System.Drawing.Point(9, 51);
+            this.linkLabel1.Name = "linkLabel1";
+            this.linkLabel1.Size = new System.Drawing.Size(183, 13);
+            this.linkLabel1.TabIndex = 12;
+            this.linkLabel1.TabStop = true;
+            this.linkLabel1.Text = "How do I get my authorization code?";
+            this.linkLabel1.VisitedLinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(184)))), ((int)(((byte)(121)))), ((int)(((byte)(184)))));
+            this.linkLabel1.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(37)))), ((int)(((byte)(37)))));
+            this.ClientSize = new System.Drawing.Size(790, 451);
+            this.Controls.Add(this.linkLabel1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.messageContainer);
+            this.Controls.Add(this.errorAuth);
+            this.Controls.Add(this.serversLabel);
+            this.Controls.Add(this.friendsLabel);
+            this.Controls.Add(this.friendsContainer);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.guildsContainer);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Font = new System.Drawing.Font("Tahoma", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(110)))), ((int)(((byte)(190)))), ((int)(((byte)(254)))));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Discord Message Purger";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.FlowLayoutPanel guildsContainer;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.FlowLayoutPanel friendsContainer;
+        private System.Windows.Forms.Label friendsLabel;
+        private System.Windows.Forms.Label serversLabel;
+        private System.Windows.Forms.Label errorAuth;
+        private System.Windows.Forms.ListView messageContainer;
+        private System.Windows.Forms.ColumnHeader Author;
+        private System.Windows.Forms.ColumnHeader Message;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.LinkLabel linkLabel1;
+    }
+}
+
---
+++ WindowsFormsApp1/Form1.cs
@@ -0,0 +1,273 @@
+﻿using Humanizer;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Net.Http;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        string authenticationToken = "";
+        private static readonly HttpClient client = new HttpClient();
+        private static String userID = string.Empty;
+        private List<(String, String)> messageIDs = new List<(String, String)>();
+        private int delay = 5000;
+        private int totalMessages = 0;
+        private int totalDMs = 0;
+        private int totalServers = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+            messageContainer.OwnerDraw = true;
+            messageContainer.DrawColumnHeader += (s, e) => {
+                using (var brush = new LinearGradientBrush(
+                    e.Bounds,
+                    Color.FromArgb(73, 73, 73),
+                    Color.FromArgb(0, 0, 0),
+                    LinearGradientMode.Vertical
+                ))
+                {
+                    e.Graphics.FillRectangle(brush, e.Bounds);
+                }
+
+                using (var format = new StringFormat())
+                {
+                    format.LineAlignment = StringAlignment.Center;
+                    e.Graphics.DrawString(e.Header.Text, e.Font, new SolidBrush(Color.FromArgb(110, 190, 254)), e.Bounds, format);
+                }
+            };
+            messageContainer.DrawItem += (s, e) => e.DrawDefault = true;
+            messageContainer.DrawSubItem += (s, e) => e.DrawDefault = true;
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            if (String.IsNullOrEmpty(authenticationToken)) return;
+            Console.WriteLine("getting");
+            friendsContainer.Enabled = false;
+            guildsContainer.Enabled = false;
+            try
+            {
+                errorAuth.Text = "";
+                client.DefaultRequestHeaders.Clear();
+                client.DefaultRequestHeaders.Add("authorization", authenticationToken);
+                friendsContainer.Controls.Clear();
+                guildsContainer.Controls.Clear();
+                button1.Enabled = false;
+                var userResponse = await client.GetAsync("https://discord.com/api/v10/users/@me");
+                var userString = await userResponse.Content.ReadAsStringAsync();
+                var userInfo = JObject.Parse(userString);
+                if (userInfo["id"] == null) throw new Exception("Check auth token and try again.");
+                userID = userInfo["id"].ToString();
+            } catch(Exception exception)
+            {
+                errorAuth.Text = exception.Message;
+                button1.Enabled = true;
+                return;
+            }
+
+            try
+            {
+                var response = await client.GetAsync("https://discord.com/api/v10/users/@me/guilds");
+                var responseString = await response.Content.ReadAsStringAsync();
+                var guilds = JArray.Parse(responseString);
+                foreach (var guild in guilds)
+                {
+                    Label guildName = new Label();
+                    guildName.Text = guild["name"].ToString();
+                    guildName.Click += (senderLocal, eLocal) => getMessagesGuild(senderLocal, eLocal, guild["id"].ToString(), guild["name"].ToString());
+                    guildsContainer.Controls.Add(guildName);
+                    serversLabel.Text = $"Servers: {(++totalServers).ToString()}";
+                }
+            }
+            catch (Exception exception) {
+                errorAuth.Text = exception.Message;
+                button1.Enabled = true;
+                return;
+            }
+
+            try
+            {
+                var response = await client.GetAsync("https://discord.com/api/v10/users/@me/relationships");
+                var responseString = await response.Content.ReadAsStringAsync();
+                var friends = JArray.Parse(responseString);
+                foreach (var friend in friends)
+                {
+                    Label friendName = new Label();
+                    if (friend["type"].ToString() != "3") friendName.Text = friend["user"]["username"].ToString();
+                    else friendName.Text = friend["name"].ToString();
+                    var friendID = friend["id"].ToString();
+                    var body = new StringContent(
+                        JsonConvert.SerializeObject(new { recipient_id = friendID }),
+                        Encoding.UTF8,
+                        "application/json"
+                    );
+                    var channelResponse = await client.PostAsync("https://discord.com/api/v10/users/@me/channels", body);
+                    var channelResponseString = await channelResponse.Content.ReadAsStringAsync();
+                    var channel = JObject.Parse(channelResponseString);
+                    friendName.Click += (senderLocal, eLocal) => getMessagesDM(senderLocal, eLocal, channel["id"].ToString(), friend["user"]["username"].ToString());
+                    friendsContainer.Controls.Add(friendName);
+                    friendsLabel.Text = $"Friends: {(++totalDMs).ToString()}";
+                }
+            }
+            catch (Exception exception)
+            {
+                errorAuth.Text = exception.Message;
+                button1.Enabled = true;
+                return;
+            }
+
+            friendsContainer.Enabled = true;
+            guildsContainer.Enabled = true;
+            button1.Enabled = true;
+        }
+
+        private async void getMessagesDM(object sender, EventArgs e, String id, String username)
+        {
+            button2.Enabled = false;
+            messageIDs.Clear();
+            messageContainer.Items.Clear();
+            label1.Text = "Getting messages... please wait";
+            int offsetIncrement = 25;
+            int offset = 0;
+            int calls = 0;
+            totalMessages = 0;
+            Console.WriteLine($"GETTING MESSAGES FROM DM");
+            guildsContainer.Enabled = false;
+            friendsContainer.Enabled = false;
+            button1.Enabled = false;
+            while (true)
+            {
+                var responseLabel = await client.GetAsync($"https://discord.com/api/v10/channels/{id}/messages/search?author_id={userID}&sort_by=timestamp&sort_order=desc&offset={offset}");
+                calls++;
+                var responseStringLabel = await responseLabel.Content.ReadAsStringAsync();
+                var messagesJson = JObject.Parse(responseStringLabel);
+                var messages = messagesJson["messages"];
+                if (messages.ToArray().Length == 0) break;
+                foreach (var messageGroup in messages)
+                {
+                    foreach (var message in messageGroup)
+                    {
+                        ListViewItem messageContent = new ListViewItem();
+                        messageContent.Tag = message["id"].ToString();
+                        messageContent.Text = message["author"]["username"].ToString();
+                        messageContent.SubItems.Add(message["content"].ToString());
+                        messageIDs.Add((message["channel_id"].ToString(), message["id"].ToString()));
+                        totalMessages++;
+                        messageContainer.Items.Add(messageContent);
+                    }
+                }
+                offset += offsetIncrement;
+                await Task.Delay(delay);
+            }
+            label1.Text = $"Total messages found in DMs with {username}: {totalMessages}";
+            if (messageIDs.Count() > 0)
+            {
+                button2.Enabled = true;
+            }
+            guildsContainer.Enabled = true;
+            friendsContainer.Enabled = true;
+            button1.Enabled = true;
+            Console.WriteLine($"ENDED {calls} CALLS");
+        }
+
+        private async void getMessagesGuild(object sender, EventArgs e, String id, String name)
+        {
+            button2.Enabled = false;
+            messageIDs.Clear();
+            messageContainer.Items.Clear();
+            label1.Text = "Getting messages... please wait";
+            int offsetIncrement = 25;
+            int offset = 0;
+            int calls = 0;
+            totalMessages = 0;
+            Console.WriteLine($"GETTING MESSAGES FROM SERVER");
+            guildsContainer.Enabled = false;
+            friendsContainer.Enabled = false;
+            button1.Enabled = false;
+            while (true)
+            {
+                var responseLabel = await client.GetAsync($"https://discord.com/api/v10/guilds/{id}/messages/search?author_id={userID}&sort_by=timestamp&sort_order=desc&offset={offset}");
+                calls++;
+                var responseStringLabel = await responseLabel.Content.ReadAsStringAsync();
+                var messagesJson = JObject.Parse(responseStringLabel);
+                var messages = messagesJson["messages"];
+                if (messages.ToArray().Length == 0) break;
+                foreach (var messageGroup in messages)
+                {
+                    foreach (var message in messageGroup)
+                    {
+                        ListViewItem messageContent = new ListViewItem();
+                        messageContent.Tag = message["id"].ToString();
+                        messageContent.Text = message["author"]["username"].ToString();
+                        messageContent.SubItems.Add(message["content"].ToString());
+                        messageIDs.Add((message["channel_id"].ToString(), message["id"].ToString()));
+                        totalMessages++;
+                        messageContainer.Items.Add(messageContent);
+                    }
+                }
+                offset += offsetIncrement;
+                await Task.Delay(delay);
+            }
+            label1.Text = $"Total messages in {name}: {totalMessages}";
+            if(messageIDs.Count() > 0)
+            {
+                button2.Enabled = true;
+            }
+            guildsContainer.Enabled = true;
+            friendsContainer.Enabled = true;
+            button1.Enabled = true;
+            Console.WriteLine($"ENDED {calls} CALLS");
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            authenticationToken = textBox1.Text;
+        }
+
+        private async void button2_Click(object sender, EventArgs e)
+        {
+            button2.Enabled = false;
+            Console.WriteLine("pressed");
+            System.Diagnostics.Stopwatch sw = new System.Diagnostics.Stopwatch();
+            try {
+                sw.Start();
+                foreach (var tuple in messageIDs.ToList())
+                {
+                    var time = TimeSpan.FromMilliseconds(delay * messageIDs.Count()).Humanize();
+                    label1.Text = $"Deleting messages... may take {time}";
+                    await client.DeleteAsync($"https://discord.com/api/v10/channels/{tuple.Item1}/messages/{tuple.Item2}");
+                    var item = messageContainer.Items.Cast<ListViewItem>().FirstOrDefault(i => i.Tag?.ToString() == tuple.Item2);
+                    messageIDs.Remove((tuple.Item1, tuple.Item2));
+                    messageContainer.Items.Remove(item);
+                    if(messageIDs.Count() > 0) await Task.Delay(delay);
+                }
+                sw.Stop();
+            } catch (Exception exception)
+            {
+                Console.WriteLine(exception.Message);
+            }
+            label1.Text = $"Deleted a total of {totalMessages} messages in {sw.Elapsed.Humanize()}.";
+            button2.Enabled = true;
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("https://gist.github.com/MarvNC/e601f3603df22f36ebd3102c501116c6");
+        }
+    }
+}
